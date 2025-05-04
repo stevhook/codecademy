85a78f9c17698e148da37af35e1b34370761c57b
stevhook <stevhook@proton.me>
2025-05-04 14:34:02 +0100

    update to core theme with script to change theme

---
+++ portfolio-projects/portfolio-website/resources/script.js
@@ -0,0 +1,18 @@
+﻿const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+    const currentTheme = document.getElementById("theme-stylesheet");
+    const currentHref = currentTheme.getAttribute("href");
+
+    // Cycle through the three themes
+    let newTheme;
+    if (currentHref === "resources/index.css") {
+        newTheme = "resources/alternative1.css";
+    } else if (currentHref === "resources/alternative1.css") {
+        newTheme = "resources/alternative2.css";
+    } else {
+        newTheme = "resources/index.css";
+    }
+    
+    currentTheme.setAttribute("href", newTheme);
+});
