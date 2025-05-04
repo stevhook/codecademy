53cd4b57454fc159f85e5fe1bdd61f8e736901f4
stevhook <stevhook@proton.me>
2025-05-04 13:27:55 +0100

    exam submission with 100% pass

--- examination-scripts/building-interactive-websites-exam-part-2/Examination scripts/script.js
+++ examination-scripts/building-interactive-websites-exam-part-2/Examination scripts/script.js
@@ -1,25 +1,32 @@
 ﻿// Click handler for search button
 const captureSearchValue = () => {
-
+    let searchInput = document.getElementById('search-bar');
+    return String(searchInput.value || '');
 };
 
 // Filter books based on search input
-const filterBooks = () => {
-
+const filterBooks = (searchTag, bookObjects) => {
+    return bookObjects.filter(book => {
+        const flattenedBooks = flattenObjectValuesIntoArray([book])[0];
+        return flattenedBooks.some(value => String(value).toLowerCase() === searchTag.toLowerCase());
+    })
 };
 
 // Empty the book list container, iterate over list of filtered books, return list of books formatted as HTML using the function in `helper.js` 
-const structureBooksAsHtml = () => {
-
+const structureBooksAsHtml = (bookArray) => {
+    return bookArray.map(book => structureBookAsHtml(book));
 };
 
 // Handler triggered when a user clickers the "Search" button. Chains previously defined functions together to filter books based on the search value, formats the books as HTML and renders them to the DOM
 const searchBtnClickHandler = () => {
-
+    const searchValue = captureSearchValue();
+    const filteredBooks = filterBooks(searchValue,books);
+    const booksAtHtml = structureBooksAsHtml(filteredBooks);
+    renderBooksToDom(booksAtHtml)
 }
 
 // Grab search button from the DOM
 
 
 // Attach an event listener to the search button
-searchBtn.addEventListener("click", () => { searchBtnClickHandler(books) });
+//document.getElementById('search-btn').addEventListener("click", () => { searchBtnClickHandler(books) });
---
+++ examination-scripts/building-interactive-websites-exam-part-2/Examination scripts/test.js
@@ -0,0 +1,73 @@
+﻿const books = [
+    {
+        title: "The City We Became",
+        author: "N. K. Jemisin",
+        tags: ["fantasy", "fiction", "afrofutursim", "science fiction", "sci-fi"]
+    },
+    {
+        title: "The Catcher in the Rye",
+        author: "J. D. Salinger",
+        tags: ["fiction", "young adult", "YA", "realism", "coming of age", "classic"]
+    },
+    {
+        title: "The Hundred Thousand Kingdoms",
+        author: "N. K. Jemisin",
+        tags: ["fantasy", "fiction", "adventure", "series"]
+    },
+    {
+        title: "Sapiens: A Brief History of Humankind",
+        author: "Yuval Noah Harari",
+        tags: ["nonfiction", "history", "anthropology", "science", "sociology"]
+    },
+    {
+        title: "Behave: The Biology of Humans at Our Best and Worst",
+        author: "Robert M. Sapolsky",
+        tags: ["nonfiction", "anthropology", "science", "sociology", "biology"]
+    },
+    {
+        title: "The Parable of the Talents",
+        author: "Octavia Butler",
+        tags: ["fiction", "dystopian", "science fiction"]
+    },
+    {
+        title: "1984",
+        author: "George Orwell",
+        tags: ["fiction", "dystopian", "science fiction", "classics", "adult"]
+    },
+    {
+        title: "Remarkably Bright Creatures",
+        author: "Shelby Van Pelt",
+        tags: ["fiction", "mystery", "magical realism"]
+    },
+    {
+        title: "Crying in H Mart",
+        author: "Michelle Zauner",
+        tags: ["memoir", "nonfiction", "autobiography"]
+    },
+    {
+        title: "Wild: From Lost to Found on the Pacific Crest Trail",
+        author: "Cheryl Strayed",
+        tags: ["nonfiction", "memoir", "adventure", "travel"]
+    }
+]
+
+const flattenObjectValuesIntoArray = (arrOfObjs) => {
+    let flattenedObj;
+    const flattenedObjsArr = [];
+    for (let obj = 0; obj < arrOfObjs.length; obj++) {
+        const objValues = Object.values(arrOfObjs[obj]);
+        flattenedObj = [...objValues.flat()]
+        flattenedObjsArr.push(flattenedObj)
+    }
+    return flattenedObjsArr;
+};
+
+const filterBooks = (searchTag, bookObjects) => {
+    const bookArray = flattenObjectValuesIntoArray(bookObjects);
+    const filteredBooks = bookArray.filter(bookDetails => {
+        bookDetails.some(detail => String(detail).toLowerCase().includes(searchTag.toLowerCase()))
+        return bookDetails.includes(searchTag)});
+    return filterBooks()
+};
+
+console.log(filterBooks("fiction", books))
