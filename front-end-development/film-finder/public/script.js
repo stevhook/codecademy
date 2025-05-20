9a7b62a6a19cb435297320e8cd92b21baad7feb0
stevhook <stevhook@proton.me>
2025-05-20 21:25:42 +0100

    getMovies implementation

--- front-end-development/film-finder/public/script.js
+++ front-end-development/film-finder/public/script.js
@@ -1,42 +1,51 @@
-﻿const tmdbKey = '9cc947a3d5f936495e305b9f41ca4182';
-const tmdbBaseUrl = 'https://api.themoviedb.org/3';
-const playBtn = document.getElementById('playBtn');
+﻿const tmdbKey = "9cc947a3d5f936495e305b9f41ca4182";
+const tmdbBaseUrl = "https://api.themoviedb.org/3";
+const playBtn = document.getElementById("playBtn");
+const genreRequestEndpoint = "/genre/movie/list";
+const discoverMovieEndpoint = "/discover/movie";
+const selectedGenre = getSelectedGenre();
 
 const getGenres = async () => {
-    const genreRequestEndpoint = '/genre/movie/list';
-    const selectedGenre = getSelectedGenre();
-    const requestParams = '?api_key='+tmdbKey
-    const urlToFetch = tmdbBaseUrl+genreRequestEndpoint+requestParams
+    const requestParams = "?api_key=" + tmdbKey;
+    const urlToFetch = tmdbBaseUrl + genreRequestEndpoint + requestParams;
     try {
-        const response = await fetch(urlToFetch)
+        const response = await fetch(urlToFetch);
         if (response.ok) {
             const jsonResponse = await response.json();
-            console.log(jsonResponse)
+            console.log(jsonResponse);
             const genres = jsonResponse.genres;
-            console.log(genres)
+            console.log(genres);
             return genres;
         }
-
-    } catch(error) {
-        console.log(error)
+    } catch (error) {
+        console.log(error);
     }
-}
-
-const getMovies = () => {
-
 };
 
-const getMovieInfo = () => {
-
+const getMovies = async () => {
+    const requestParams = "?api_key=" + tmdbKey + "&with_genres=" + selectedGenre;
+    const urlToFetch = tmdbBaseUrl + discoverMovieEndpoint + requestParams;
+    try {
+        const response = await fetch(urlToFetch);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            console.log(jsonResponse);
+            const movies = jsonResponse.results;
+            return movies;
+        }
+    } catch(error) {
+        console.log(error);
+    }
 };
+
+const getMovieInfo = () => {};
 
 // Gets a list of movies and ultimately displays the info of a random movie from the list
 const showRandomMovie = () => {
-    const movieInfo = document.getElementById('movieInfo');
+    const movieInfo = document.getElementById("movieInfo");
     if (movieInfo.childNodes.length > 0) {
         clearCurrentMovie();
-    };
-
+    }
 };
 
 getGenres().then(populateGenreDropdown);
